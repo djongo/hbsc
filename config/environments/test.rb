3032283eeeb0d54459486501aa9a703f31f1f462
djongo <jonasholstein@gmail.com>
2010-09-10 17:59:33 +0200

    Solved problems with nifty-generators + installed testing gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,7 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+  config.gem 'rspec', :source => 'http://gemcutter.org'
+  config.gem 'rspec-rails', :source => 'http://gemcutter.org'
+  config.gem 'webrat', :source => 'http://gemcutter.org'
+  
