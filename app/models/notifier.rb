9a8609200a51af5db2cd010a40384921e78c84d4
djongo <jonasholstein@gmail.com>
2010-09-11 15:41:54 +0200

    Changed notifier error

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,9 +1,12 @@
 class Notifier < ActionMailer::Base
+
   def password_reset_instructions(user)
     subject     "Password Reset Instructions for HBSC"
     from        "Puma HBSC"
-    recipients  user.email
-    sent_on     Time.now
-    body        :edit_password_reset_url => edit_password_reset_url(user.persistence_token)
+    recipients   user.email
+    content_type "text/html"
+    sent_on      Time.now
+    body         :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
   end
+
 end
