d365d8180ab331fe060ff870dc26bbd7d159aa73
djongo <jonasholstein@gmail.com>
2010-09-11 15:56:47 +0200

    Changed environment to reflect plugin

--- config/environment.rb
+++ config/environment.rb
@@ -45,13 +45,13 @@ Rails::Initializer.run do |config|
   # mail settings
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }    
-  
-  config.action_mailer.smtp_settings = {
-    :address  => "smtp.gmail.com",
-    :port  => "587",
-    :user_name  => "puma.hbsc@gmail.com",
-    :password  => "lrnbijxp",
-    :authentication  => :login
-  }
+#  config.action_mailer.default_url_options = { :host => "localhost:3000" }    
+#  
+#  config.action_mailer.smtp_settings = {
+#    :address  => "smtp.gmail.com",
+#    :port  => "587",
+#    :user_name  => "puma.hbsc@gmail.com",
+#    :password  => "lrnbijxp",
+#    :authentication  => :login
+#  }
 end
