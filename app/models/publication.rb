fbc0e76418afc1c1bb1ac99ee3f7b659c7db2670
djongo <jonasholstein@gmail.com>
2010-09-12 13:58:02 +0200

    Ready for acts as indexed

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,5 +1,8 @@
 class Publication < ActiveRecord::Base
   attr_accessible :title, :description, :language_id, :publication_type_id
+
+  acts_as_indexed :fields => [:title, :description ]
+
   belongs_to :language
   belongs_to :publication_type
   has_many :keywords
