a81daf41278d714d1610250f7f483cfec2e3b7a8
djongo <jonasholstein@gmail.com>
2010-09-13 22:14:41 +0200

    adding the trick

---
+++ config/initializers/pdfkit.rb
@@ -0,0 +1,8 @@
+# config/initializers/pdfkit.rb
+
+PDFKit.configure do |config|     
+
+config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
+
+end
+
