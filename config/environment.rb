80ccfbec75118ac5dcc429ae69594f1b868c503b
djongo <jonasholstein@gmail.com>
2010-11-23 21:32:58 +0100

    changing back to rails 2.3.8

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.8' # unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' # unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
 
-#  config.gem 'rails', :source => 'http://gemcutter.org', :version => '2.3.8'
+  config.gem 'rails', :source => 'http://gemcutter.org', :version => '2.3.8'
   config.gem 'authlogic', :source => 'http://gemcutter.org'
   config.gem 'declarative_authorization', :source => 'http://gemcutter.org'
   config.gem 'will_paginate', :source => 'http://gemcutter.org'
