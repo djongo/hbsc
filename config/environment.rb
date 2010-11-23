16ddcc3fd6655355fefe4fd7299be35efb4ff2ee
djongo <jonasholstein@gmail.com>
2010-11-23 21:24:35 +0100

    Fiddling more with rails version

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
 
-  config.gem 'rails', :source => 'http://gemcutter.org', :version => '2.3.8'
+#  config.gem 'rails', :source => 'http://gemcutter.org', :version => '2.3.8'
   config.gem 'authlogic', :source => 'http://gemcutter.org'
   config.gem 'declarative_authorization', :source => 'http://gemcutter.org'
   config.gem 'will_paginate', :source => 'http://gemcutter.org'
