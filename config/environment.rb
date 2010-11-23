8a8e27e2e0918f49c338c3687b1a3d371578ab45
djongo <jonasholstein@gmail.com>
2010-11-23 22:17:46 +0100

    Trying to ignore dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'acts_as_indexed', :source => 'http://gemcutter.org'
   config.gem 'pdfkit', :source => 'http://gemcutter.org'
   config.gem 'aasm', :source => 'http://gemcutter.org'
-  config.gem 'paper_trail', :source => 'http://gemcutter.org', :version => '1.6.2'    
+  config.gem 'paper_trail', :source => 'http://gemcutter.org'
     
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
