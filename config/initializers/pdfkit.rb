5ef8a95848710f35c361b9291b280037eb837351
Jonas Holstein <jonasholstein@gmail.com>
2012-02-19 13:39:58 +0100

    rollback pdfkit change

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -3,7 +3,7 @@
 PDFKit.configure do |config|     
 # bin was vendor
 # http://blog.mattgornick.com/using-pdfkit-on-heroku
-config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
+config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
 
 end
 
