e77c4c79424e1304bf57d7fafde7dc71dcfeda92
djongo <jonasholstein@gmail.com>
2012-02-19 13:16:23 +0100

    trying pdfkit fix

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,9 @@
 # config/initializers/pdfkit.rb
 
 PDFKit.configure do |config|     
-
-config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
+# bin was vendor
+# http://blog.mattgornick.com/using-pdfkit-on-heroku
+config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
 
 end
 
