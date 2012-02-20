153d03e7c6bd9ca3bd072ba878b8495d266b0682
Jonas Holstein <jonasholstein@gmail.com>
2012-02-20 20:38:27 +0100

    change in mime

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,6 +3,14 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-# Mime::Type.register "application/pdf", :pdf
+Mime::Type.register "application/pdf", :pdf
 
 Mime::Type.register "application/vnd.ms-excel", :xls
+
+format.pdf do
+  kit = PDFKit.new(render_to_string(:template => "publications/show.html",
+                                    :layout => "views/layouts/application.html.erb"))
+  # stylesheets in tmp because I'm using compass
+  kit.stylesheets << Rails.root.join('public', 'stylesheets', 'application.css').to_s
+  render :text => kit.to_pdf
+end
