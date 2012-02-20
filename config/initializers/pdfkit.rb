4afd04debc2b0f8c2e22667c7512d7762d31a4ec
Jonas Holstein <jonasholstein@gmail.com>
2012-02-20 22:29:25 +0100

    Changed pdfkit.configure

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -3,8 +3,8 @@
 PDFKit.configure do |config|     
 # bin was vendor
 # http://blog.mattgornick.com/using-pdfkit-on-heroku
-# config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
+ config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
 # https://gist.github.com/768696#file_assets.rb
-  config.root_url = "file://#(Rails.root.join('public'))/" if RAILS_ENV=='production'
+  config.root_url = "file://#(Rails.root.join('public'))/" if RAILS_ENV == 'production'
 end
 
