ef469996b21b95685fd52a62ae17eb18dd08d376
Jonas Holstein <jonasholstein@gmail.com>
2012-02-22 20:03:42 +0100

    More pdfkit

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,10 +1,11 @@
 # config/initializers/pdfkit.rb
 
 PDFKit.configure do |config|     
-# bin was vendor
-# http://blog.mattgornick.com/using-pdfkit-on-heroku
  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
-# https://gist.github.com/768696#file_assets.rb
+  config.default_options = {
+    :page_size => 'A4',
+    :print_media_type => true
+    }
   config.root_url = "file://#(Rails.root.join('public'))/" if RAILS_ENV == 'production'
 end
 
